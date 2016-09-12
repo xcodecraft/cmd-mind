4fa50efea34fdefdf9719d540eecb261799a637d
左文建 <zuowenjian@mararun.com>
2016-09-12 16:15:19 +0800

    work

---
+++ src/impl/parser.py
@@ -0,0 +1,8 @@
+#coding=utf8
+
+
+def parse(input):
+
+class parse_status:
+
+    pass
---
+++ test/testcase/parse_tc.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+import  re , os , string ,  getopt ,sys , unittest,logging
+
+
+class parse_tc(unittest.TestCase):
+    def test_parse(self):
+        input = "test all --prj pylon --env dev"
+        parser.pase(input)
+
